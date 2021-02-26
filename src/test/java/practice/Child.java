87072188c4dcde0f004dbfd11aa17c540dd82174
May <yanjiemay69@gmail.com>
2021-02-25 19:56:34 -0500

    Hi
    Hzee

---
+++ src/test/java/practice/Child.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Child extends Parent{
+	
+	public static void main(String[] args) {
+		
+		testOverride();
+	}
+	
+	public static void testOverride2(){
+		System.out.println("");
+	}
+}
---
+++ src/test/java/practice/Parent.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Parent {
+	
+	public static void testOverride() {
+		System.out.println("");
+	}
+	
+	
+	public static void startTest() {
+		
+		
+	}
+
+}
