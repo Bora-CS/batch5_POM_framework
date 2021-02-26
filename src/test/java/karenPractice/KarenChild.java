d79c3892ca9affa5e9ac404c9fc321726685ec36
Karen <yangxiaotong0412@gmail.com>
2021-02-25 20:01:21 -0500

    new

---
+++ src/test/java/karenPractice/KarenChild.java
@@ -0,0 +1,15 @@
+package karenPractice;
+
+public class KarenChild extends KarenParent{
+
+	
+	
+	public static void main(String[] args) {
+		
+		testOverRide();
+	}
+	
+	public static void testOverRide() {
+		System.out.println("Hello~ Child Class");
+	}
+}
